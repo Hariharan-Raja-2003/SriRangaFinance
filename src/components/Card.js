a77c895e2f98792965fb9c41821dec49ea44db6b
Hariharan <143105466+Hariharan-Raja-2003@users.noreply.github.com>
2025-11-21 13:42:51 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,23 @@ function Card({ title, img, text }) {
   const handleImageError = (e) => {
     e.target.src =
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrrvS1K5cZnlv1bdNqogdqgMNz8Rlu_GWXJw&s";
-    e.target.style.objectFit = "cover";
   };
 
   return (
     <div className="card">
       <div className="card-image-wrapper">
         <img
-          alt={title}
           src={img}
+          alt={title}
           className="card-image"
-          loading="lazy"
           onError={handleImageError}
+          loading="lazy"
         />
       </div>
 
       <div className="card-content p-3">
         <h3 className="card-title text-center">{title}</h3>
-
-        <p className="card-text mt-2">{text}</p>
+        <p className="card-text">{text}</p>
       </div>
     </div>
   );
